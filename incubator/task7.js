1aac63b55ccf9f77f1460ac707de6d6d24a28d43
T1MUR <104462000+t1murgareev@users.noreply.github.com>
2022-06-04 19:17:56 +0300

    Add files via upload

---
+++ incubator/task7.js
@@ -0,0 +1,10 @@
+function contamination(text, char){
+    let i = text.length
+    if (text.length > 0 && text.length > 0) {
+        return char.repeat(i)
+    }else {
+        return ""
+    }
+}
+
+console.log(contamination("abc","z"))
